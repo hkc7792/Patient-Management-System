afb1d9cb87f1fcb85d342d62e93eb8cccaeebf7f
Hariday Kannoujia <hkc7792@gmail.com>
2026-09-04 14:18:11 +0530

    jpa auditing added

---
+++ Patient-Service/src/main/java/com/app/patient/patientservice/config/JpaAuditingConfig.java
@@ -0,0 +1,21 @@
+package com.app.patient.patientservice.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+
+//Enable JPA auditing
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("system"); // placeholder until Cognito/Auth is wired in
+    }
+}
